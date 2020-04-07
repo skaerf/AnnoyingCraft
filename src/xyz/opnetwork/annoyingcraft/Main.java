c322d36af1d379f52faeeac6f5ca15d22e2025c3
skaerf <lawrence.harrison@outlook.com>
2020-04-07 15:32:22 +0100

    template commit

--- src/xyz/opnetwork/annoyingcraft/Main.java
+++ src/xyz/opnetwork/annoyingcraft/Main.java
@@ -1,5 +1,18 @@
 package xyz.opnetwork.annoyingcraft;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.println("[AnnoyingCraft] BOOTING");
+		System.out.println("[AnnoyingCraft] -------------------------------------------------------------");
+		System.out.println("[AnnoyingCraft] Need support? Join https://discord.skaerf.xyz for assistance!");
+		System.out.println("[AnnoyingCraft] -------------------------------------------------------------");
+	}
+	public void onDisable() {
+		System.out.println("[AnnoyingCraft] SHUTTING DOWN");
+	}
 
 }
